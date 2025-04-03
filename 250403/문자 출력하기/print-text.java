e4609ac4ff209a23be7fb0b3f2902ea19198a347
sangjaekwon <sjsj00718@gmail.com>
2025-04-03 12:55:29 +0900

    Title: 문자 출력하기, Time: 108ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250403/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String ch = scanner.nextLine();
+
+        for(int i =0; i< 8; i++) {
+            System.out.print(ch);
+        }
+        // Please write your code here.
+    }
+}
