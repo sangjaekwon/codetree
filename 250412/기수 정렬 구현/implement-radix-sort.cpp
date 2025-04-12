821ca5ab9d3538a0ddbf3e02a8475de5c6a0a40f
sangjaekwon <sjsj00718@gmail.com>
2025-04-12 20:06:05 +0900

    Title: 기수 정렬 구현, Time: 13ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250412/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int arr[100000];
+
+void radix_sort(int arr2[], int count) {
+    for(int k = count; k > 0; k--) {
+        vector<int> arr_new[10];
+        for(int i = 0; i< n; i++) {
+            int posth;
+            for(int j = k; j >1 ; j--) {
+                posth = arr2[i] / 10;
+            }
+            int digit = arr2[i] % 10;
+            arr_new[digit].push_back(arr[i]);
+        }
+        int storage_arr[n];
+        int c = 0;
+        for(int i =0; i<10; i++) {
+            for(int j = 0; j<arr_new[i].size(); j++) {
+                storage_arr[c] = arr_new[i].at(j);
+                c++;
+            }
+        }
+
+        for(int i =0; i< n; i++) {
+            arr[i] = storage_arr[i];
+        }
+
+
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int max =0;
+    for(int i =0; i< n; i++) {
+        if( arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    int count= 1;
+    while( max / 10 != 0) {
+        max /= 10;
+        count++;
+    }
+    radix_sort(arr, count);
+    // Please write your code here.
+    for(int i = 0; i< n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
