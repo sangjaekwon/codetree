a3da9c2eaaad0afdc5c62c952e1752dae4029578
sangjaekwon <sjsj00718@gmail.com>
2025-04-22 14:04:47 +0900

    Title: 퀵 정렬 구현, Time: 35ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250422/퀵 정렬 구현/implement-quick-sort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100000];
+
+int partition(int low, int high) {
+    int pivot = arr[high];
+    int i = low-1;
+
+    for(int j =low; j<high; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[high]);
+
+    return i+1;
+}
+
+
+
+void quickSort(int low, int high) {
+    if(low < high) {
+        int pos = partition(low, high);
+        quickSort(low, pos-1);
+        quickSort(pos+1, high);
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    quickSort(0, n-1);
+    // Please write your code here.
+    for(int i =0; i< n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
